1c901d55c1ee25ca2424d02b4bc4cea0b90b88d9
Bryan <bryanmacieldealmeida06@gmail.com>
2025-06-11 00:45:52 -0300

    adicionado: - dados json - scripts, styles e json separados para melhor organização - design inicial para entradas

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-const cardapioContainer = document.querySelector('#cardapioContainer')
+const nav = document.querySelector('nav');
+const cardapioBtn = document.querySelector('.cardapio-btn');
+
+cardapioBtn.addEventListener("click", function () {
+    nav.classList.toggle("nav-aberto");
+    nav.classList.toggle("nav-fechado");
+});
+
+// Fecha ao clicar fora do nav
+document.addEventListener("click", function (event) {
+    const isClickInsideNav = nav.contains(event.target);
+    const isClickOnButton = cardapioBtn.contains(event.target);
+
+    if (!isClickInsideNav && !isClickOnButton) {
+        if (nav.classList.contains("nav-aberto")) {
+            nav.classList.remove("nav-aberto");
+            nav.classList.add("nav-fechado");
+        }
+    }
+});
